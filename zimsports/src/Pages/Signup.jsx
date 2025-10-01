5ac433aa9285fe71f62a588c44646933a637f791
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-10-01 23:02:32 +0500

    login-updated

--- zimsports/src/Pages/Signup.jsx
+++ zimsports/src/Pages/Signup.jsx
@@ -5,8 +5,11 @@ import "../assets/CSS/Signup.css"
 export default function Signup() {
   return (
     <>
-    
- <h1>Sign up form</h1>
+    <div className="container-fluid p-0 m-0">
+      <div className="banner">
+        <div cren="Registration"></div>
+      </div>
+    </div>
     </>
   );
 }
