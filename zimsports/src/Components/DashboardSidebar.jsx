7e272bc4e54cbb2f6c933bd9dea7a3bab6053b23
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-10-08 22:44:41 +0500

    updated code

--- zimsports/src/Components/DashboardSidebar.jsx
+++ zimsports/src/Components/DashboardSidebar.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { Home, Users, Trophy, UserCheck, User } from "lucide-react";
 import "../assets/CSS/DashboardSidebar.css";
 export default function DashboardSidebar() {
   return (
- <>
-  {/* Sidebar */}
-     <aside className="sidebar">
-      <ul>
-        <li data-tooltip="Home">
-          <Link></Link>
-          <Home size={20} />
-        </li>
-        <li data-tooltip="Teams">
-          <Users size={20} />
-        </li>
-        <li data-tooltip="Matches">
-          <Trophy size={20} />
-        </li>
-        <li data-tooltip="Players">
-          <User size={20} />
-        </li>
-        <li data-tooltip="Coaches">
-          <UserCheck size={20} />
-        </li>
-      </ul>
-    </aside>
- </>
-  )
+    <>
+      {/* Sidebar */}
+      <aside className="sidebar">
+        <ul>
+          <li data-tooltip="Home">
+            <Link to="/dashboard" style={{ color: "inherit", textDecoration: "none" }}>
+              <Home size={20} />
+            </Link>
+          </li>
+          <li data-tooltip="Teams">
+            <Link to="/team" style={{ color: "inherit", textDecoration: "none" }}>
+              <Users size={20} />
+            </Link>
+          </li>
+          <li data-tooltip="Matches">
+            <Link to="/matches" style={{ color: "inherit", textDecoration: "none" }}>
+              <Trophy size={20} />
+            </Link>
+          </li>
+          <li data-tooltip="Players">
+            <Link to="/players" style={{ color: "inherit", textDecoration: "none" }}>
+              <User size={20} />
+            </Link>
+          </li>
+          <li data-tooltip="Coaches">
+            <Link to="/coaches" style={{ color: "inherit", textDecoration: "none" }}>
+              <UserCheck size={20} />
+            </Link>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
 }
